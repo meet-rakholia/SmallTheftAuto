ef818eadcb556dd3e3ee8b7b7fc5aedce09c3f6c
meet-rakholia <meet@student.forsbergsskola.se>
2023-10-17 22:28:36 +0200

    attached animator to the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,9 +9,12 @@ public class PlayerMovement : MonoBehaviour
     
     // Start is called before the first frame update
     public int playerRunningSpeed = 1;
+    private Animator _animator;
+    private static readonly int MoveX = Animator.StringToHash("MoveX");
+
     void Start()
     {
-        
+        _animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,8 +25,12 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         
         
+        
         Vector3 movement = new Vector3(horizontalInput,verticalInput, 0.0f) * playerRunningSpeed;
         this.transform.Translate(movement*Time.deltaTime);
+        _animator.SetFloat("MoveX",horizontalInput);
+        _animator.SetFloat("MoveY",verticalInput);
+        _animator.SetBool("isMoving",movement.magnitude > 0);
     }
     
 }
