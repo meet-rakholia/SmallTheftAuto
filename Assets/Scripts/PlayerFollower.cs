6c8308e6f157c5cf46905fa7ca984c38059a80d4
meet-rakholia <meet@student.forsbergsskola.se>
2023-10-18 12:04:36 +0200

    camera follows the player

--- Assets/Scripts/PlayerFollower.cs
+++ Assets/Scripts/PlayerFollower.cs
@@ -1,9 +1,21 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class PlayerFollower : MonoBehaviour
     {
-        
+        public GameObject player;
+        private Vector3 offset;
+        private void Start()
+        {
+            offset = this.transform.position - player.transform.position;
+        }
+
+        private void Update()
+        {
+            this.transform.SetPositionAndRotation(player.transform.position+offset,Quaternion.Euler(0,0,0));
+        }
     }
 }
