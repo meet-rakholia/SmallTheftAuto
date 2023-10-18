8dfbb3bc54fff455abac3ee4e0b567709290fd8a
meet-rakholia <meet@student.forsbergsskola.se>
2023-10-18 17:02:04 +0200

    started modifying the player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     public float playerRunningSpeed = 1.0f;
     public float playerTurnRate = 30.0f;
-    public float vehicleRunningSpeed = 5.0f;
+    public float vehicleAcceleration = 1.0f;
+    public float maxVehicleSpeed = 5.0f;
     public float vehicleTurnRate = 200.0f;
     private Animator _animator; 
     private static readonly int IsMoving = Animator.StringToHash("isMoving");
@@ -29,30 +30,31 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-        float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
         float turnRate;
-        float runningSpeed;
 
         if (_player.isInVehicle)
         {
             turnRate = vehicleTurnRate;
-            runningSpeed = vehicleRunningSpeed;
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                
+            }
+            
         }
         else
         {
+            float verticalInput = Input.GetAxis("Vertical");
             turnRate = playerTurnRate;
-            runningSpeed = playerRunningSpeed;
+    
+            Vector3 movement = new Vector3(0.0f,verticalInput, 0.0f) * playerRunningSpeed;
+            this.transform.Translate(movement*Time.deltaTime);
+            _animator.SetBool(IsMoving,movement.magnitude > 0);
         }
-
-
+        
         Vector3 currentOrientation = this.transform.eulerAngles;
         float newRotation = currentOrientation.z - horizontalInput*turnRate * Time.deltaTime;
         this.transform.rotation = UnityEngine.Quaternion.Euler(0.0f,0.0f,newRotation);
-    
-        Vector3 movement = new Vector3(0.0f,verticalInput, 0.0f) * runningSpeed;
-        this.transform.Translate(movement*Time.deltaTime);
-        _animator.SetBool(IsMoving,movement.magnitude > 0);
 
     }
     
