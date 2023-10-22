b69f573a2834920cc5e417357c1a5bb0e1f7f11f
meet-rakholia <meet@student.forsbergsskola.se>
2023-10-22 22:14:50 +0200

    updated player health icon update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,16 +11,26 @@ namespace DefaultNamespace
         public bool isInVehicle;
         public int health = 50;
         private Canvas _healthCanvas;
-        private Image[] _health;
+        private Image[] _health = new Image[5];
         private readonly string _spritePathNoHealth = "Sprites/Player/NoHealth";
         private readonly string _spritePathHalfHealth = "Sprites/Player/HalfHealth";
         private readonly string _spritePathFullHealth = "Sprites/Player/FullHealth";
 
         private void Start()
         {
-            GameObject canvasObject = GameObject.Find("PlayerHealthCanvas");
+            GameObject canvasObject = GameObject.Find("Canvas");
             _healthCanvas = canvasObject.GetComponent<Canvas>();
-            _health = _healthCanvas.GetComponentsInChildren<Image>();
+            Image[] allImages = _healthCanvas.GetComponentsInChildren<Image>();
+            int j = 0;
+            for (int i = 0; i < allImages.Length; i++)
+            {
+                if (allImages[i].name.Contains("PlayerHealth"))
+                {
+                    _health[j] = allImages[i];
+                    Debug.Log(_health[j].name);
+                    j++;
+                }
+            }
             UpdatePlayerHealthIcons();
         }
 
@@ -36,85 +48,94 @@ namespace DefaultNamespace
 
         private void UpdatePlayerHealthIcons()
         {
-            switch (health)
+            if (health == 100)
             {
-                case 100:
-                    foreach (var t in _health)
-                    {
-                        t.sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    }
-                    break;
-                case 90:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    break;
-                case 80:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    break;
-                case 70:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    break;
-                case 60:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    break;
-                case 50:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    break;
-                case 40:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    break;
-                case 30:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    break;
-                case 20:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
-                    break;
-                case 10:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
-                    break;
-                default:
-                    _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[3].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    _health[4].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
-                    break;
+                foreach (var t in _health)
+                {
+                    t.sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                }
             }
+            else if (health is >= 90 and < 100)
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+            }
+            else if (health is >= 80 and < 90)
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+            }
+            else if (health is >= 70 and < 80)
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+            }
+            else if (health is >= 60 and < 70)
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+            }
+            else if (health is >= 50 and < 60)
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+            }
+            else if (health is >= 40 and < 50)
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+            }
+            else if (health is >= 30 and < 40)
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+            }
+            else if (health is >= 20 and < 30)
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathFullHealth);
+            }
+            else if (health is >= 10 and < 20)
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathHalfHealth);
+            }
+            else
+            {
+                _health[0].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[1].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[2].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[3].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+                _health[4].sprite = Resources.Load<Sprite>(_spritePathNoHealth);
+            }
+
         }
     }
 }
