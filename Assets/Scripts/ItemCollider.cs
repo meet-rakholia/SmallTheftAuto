33a6bcbc1927762d090b257fa61041735072d38a
meet-rakholia <meet@student.forsbergsskola.se>
2023-10-29 21:51:43 +0100

    trying to add player pick up iitems

--- Assets/Scripts/ItemCollider.cs
+++ Assets/Scripts/ItemCollider.cs
@@ -10,13 +10,20 @@ namespace DefaultNamespace
         {
             if (other.gameObject.name == "Player")
             {
+                GameObject itemGameObject = other.gameObject.transform.Find("Item").gameObject;
+                SpriteRenderer playerItemSprite = itemGameObject.GetComponent<SpriteRenderer>();
+                SpriteRenderer itemSprite = this.GetComponent<SpriteRenderer>();
                 Player player = other.GetComponent<Player>();
                 Item item = this.gameObject.GetComponent<Item>();
                 for (int i = 0; i < player._items.Length; i++)
                 {
+                    Debug.Log(playerItemSprite.color);
+                    Debug.Log(itemSprite.color);
                     if (player._items[i] == null)
                     {
                         player._items[i] = item;
+                        playerItemSprite.color = itemSprite.color;
+                        playerItemSprite.enabled = true;
                         break;
                     }
                 }
